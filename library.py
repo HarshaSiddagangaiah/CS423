c99074b3cef8331fbfa8b67fb0fb7dda53592d6a
Harsha Siddagangaiah <100118598+HarshaSiddagangaiah@users.noreply.github.com>
2022-10-19 07:58:40 -0700

    Update library.py

--- library.py
+++ library.py
@@ -87,7 +87,7 @@ class OHETransformer(BaseEstimator, TransformerMixin):
         result = self.transform(X)
         return result
       
- class Sigma3Transformer(BaseEstimator, TransformerMixin):
+class Sigma3Transformer(BaseEstimator, TransformerMixin):
 
     def __init__(self, column_name):
         self.column_name = column_name  
